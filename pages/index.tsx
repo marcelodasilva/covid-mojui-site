0c6593b2a51f88f37ebf241087eef76ec6522106
Marcelo da Silva <marcelosilva.mdc@gmail.com>
2020-05-07 01:11:42 -0300

    typescript home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import Head from 'next/head'
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
@@ -12,3 +13,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
